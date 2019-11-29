0b631f10c2ed260dfa068c87e087453a42cb6373
Ida Li <ida.li@nrm.se>
2019-11-29 14:30:43 +0100

    Add constructor for testing

--- dina-dnakey-logic/src/main/java/se/nrm/dina/dnakey/logic/GenbankBlaster.java
+++ dina-dnakey-logic/src/main/java/se/nrm/dina/dnakey/logic/GenbankBlaster.java
@@ -10,20 +10,28 @@ import org.biojava.nbio.ws.alignment.qblast.NCBIQBlastService;
 /**
  *
  * @author idali
- */
-//@Stateless
+ */ 
 @Slf4j
 public class GenbankBlaster implements Serializable {
 
-  private NCBIQBlastService service;
-  private NCBIQBlastAlignmentProperties props;
+  private final NCBIQBlastService service;
+  private final NCBIQBlastAlignmentProperties props;
   private final String newLine = "\n";
   private final String replaceChars = "%0A";
   private final String nr = "nr";
-
+ 
   public GenbankBlaster() {
+    service = new NCBIQBlastService();
+    props = new NCBIQBlastAlignmentProperties(); 
+    // set alignment options 
+    props.setBlastProgram(BlastProgramEnum.blastn);
+    props.setBlastDatabase(nr);
   }
 
+  public GenbankBlaster(NCBIQBlastService service, NCBIQBlastAlignmentProperties props) {
+    this.service = service; 
+    this.props = props;
+  }
   /**
    * Blast from remote genbank database
    * 
@@ -32,16 +40,12 @@ public class GenbankBlaster implements Serializable {
    */
   public String remoteGenbankBlast(String fastSequence) {
     log.info("remoteGenbankBlast");
-    service = new NCBIQBlastService();
-    props = new NCBIQBlastAlignmentProperties();
-    
-    // set alignment options 
-    props.setBlastProgram(BlastProgramEnum.blastn);
-    props.setBlastDatabase(nr);
+     
     try { 
-      fastSequence = StringUtils.replace(fastSequence, newLine, replaceChars);
+      fastSequence = StringUtils.replace(fastSequence, newLine, replaceChars); 
       return service.sendAlignmentRequest(fastSequence, props);
-    } catch (Exception ex) {
+    } catch (Exception ex) { 
+      log.error(ex.getMessage());
       return null;
     }
   }
